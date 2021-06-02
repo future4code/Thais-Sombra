bf89c2b028b593c7415f501868f10703b7cd41cc
tshadz <thais.sombra@hotmail.com>
2021-06-02 14:51:31 -0300

    :package: Implantação de funcionalidade curtir e descurtir

--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -49,8 +49,15 @@ class Post extends React.Component {
   }
 
   onClickCurtida = () => {
-    console.log('Curtiu!')
-  }
+    if (this.state.curtido){
+      this.setState({numeroCurtidas: this.state.numeroCurtidas - 1})
+    } else {
+      this.setState({numeroCurtidas: this.state.numeroCurtidas + 1})
+    }
+    this.setState({
+      curtido:!this.state.curtido,
+    })
+    }
 
   onClickComentario = () => {
     this.setState({
