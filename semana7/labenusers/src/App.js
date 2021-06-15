abc8926d0fd9332fdf9f4d1102057b668dc99f4b
tshadz <thais.sombra@hotmail.com>
2021-06-15 14:02:17 -0300

    :package: Implementação de funcionalidade de adição de usuario e renderização na tela com a lista de users cadastrados

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import React from 'react';
-import styled from 'styled-components'
-import { FaUserAstronaut } from 'react-icons/fa'
+import styled from 'styled-components';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+import { IoCloseCircle } from 'react-icons/io5'
 import axios from 'axios';
 
 
@@ -13,8 +15,56 @@ const MainContainer = styled.div`
 
 const Main = styled.div`
   background-color: #282c34;
-  height: 100vh;
+  height: 50vh;
 `
+
+const UserForm = styled.div`
+    height: 250px;
+    display:flex;
+    flex-direction:column;
+    text-align: center;
+    justify-content: space-between;
+    align-items: center;
+    `
+
+const Campo = styled.div`
+    display:flex;
+    background-color: white;
+    color:darkgray;
+    border-radius: 5px;
+    height: 50px;
+    width: 300px;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    `
+
+const Input = styled.input`
+    border:none;
+    width:100%;
+    padding-left: 0px;
+    font-size: 1.2rem;
+    border-style: none;
+`
+
+const Icones = styled.i`
+    margin: 15px;
+`
+
+const BotaoLogar = styled.button`
+    background-color: white;
+    color: black;
+    text-transform: uppercase;
+    outline: none;
+    border:none;
+    padding: .78rem 1.5rem;
+    font-weight:700;
+    text-align: center;
+    margin: 30px;
+    border-radius: 5px;
+    cursor: pointer;
+`
+
 const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users'
 
 const headers = {
@@ -27,17 +77,66 @@ export default class App extends React.Component {
 
 
   state = {
-    usersList: [
-      {
-      name: '',
-      email: '',
-      }
-    ],
+    usersList: [],
     inputUserName:'',
     inputUserEmail:'',    
   }
 
+  onChangeUserName = (event) => {
+    this.setState({inputUserName: event.target.value})
+  }
+
+  onChangeUserEmail = (event) => {
+    this.setState({inputUserEmail: event.target.value})
+  }
+
+  componentDidMount () {
+    this.getAllUsers();
+  }
+
+  createUser = () => {
+    const body = {
+      name: this.state.inputUserName,
+      email: this.state.inputUserEmail
+    };
+
+    axios.post(url, body, headers)
+      .then((res) => {
+        res = "Usuário cadastrado com sucesso!"
+        alert(res);
+        this.setState({ 
+          inputUserName: '',
+          inputUserEmail: '' 
+        })
+        this.getAllUers();
+      })
+      .catch((err) => {
+        alert(err.response.data)
+      })
+  }
+
+  getAllUsers = () => {
+    axios.get(url, headers)
+      .then((res) => {
+        this.setState({ usersList: res.data })
+      })
+      .catch((err) => {
+        alert(err.response.data)
+      })
+  }
+
+  deleUSer = () => {
+    const urlDel = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/:id"
+    axios.delete(urlDel,headers)
+  }
+
   render () {
+    const userList = this.state.usersList.map((user) => {
+    return (
+    <Campo>
+      <p key={user.id}>{user.name} <IoCloseCircle onClick={this.deleteUser} /></p>
+    </Campo>
+    )})
     return (
       <MainContainer>
         <header className="App-header">
@@ -54,8 +153,30 @@ export default class App extends React.Component {
             Api Labenusers
           </a>
         </header>
+        <UserForm>
+                    <Campo>
+                        <Icones> <FaUserAstronaut /> </Icones>
+                        <Input 
+                            type="text" 
+                            placeholder='Nome'
+                            value={this.state.inputUserName}
+                            onChange={this.onChangeUserName}
+                        />
+                    </Campo>
+                    <Campo>
+                        <Icones> <FiMail /> </Icones>
+                        <Input
+                            type="text" 
+                            placeholder='e-mail'
+                            value={this.state.inputUserEmail}
+                            onChange={this.onChangeUserEmail}
+                        />
+                    </Campo>
+                    <BotaoLogar onClick={this.createUser} >Salvar</BotaoLogar>
+        </UserForm>
         <Main>
-
+          <h2>Usuários Cadastrados</h2>
+        {userList}
         </Main>
       </MainContainer>
     )
