475113f5057ba1f35c7e138903b539947ec49c13
tshadz <thais.sombra@hotmail.com>
2021-06-18 14:20:29 -0300

    :package: Filtro de listas

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -1,19 +1,70 @@
 import './App.css';
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import {RiNeteaseCloudMusicFill} from 'react-icons/ri';
 import PlaylistsContainer from './components/PlaylistsContainer'
 import Playlist from './components/Playlist';
 
+
+const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists'
+
+const headers = {
+  headers: {
+    Authorization: 'thais-sombra-molina'
+  }
+}
+
 export default class App extends React.Component {
+
+  state = {
+    allPlayLists: [],  
+    playlistName: '',  
+  }
+
+  componentDidMount () {
+    this.getAllPlaylists()  
+  }
+
+  handleFieldChange = event => {     
+    this.setState({[event.target.name]:event.target.value})  
+    console.log(event.target.value)
+  }
+
+  createPlaylist = () => {
+    const body = {
+      name:this.state.playlistName,      
+    }
+    axios
+      .post(url,body,headers)
+  }
+
+  getAllPlaylists = () => {
+    axios
+      .get(url, headers)
+      .then((res) => {
+        this.setState({ allPlayLists: res.data.result.list })
+        console.log(this.state.allPlayLists)
+      })
+      .catch((err) => {
+        alert(err.response.data.message)
+      })
+  }
+
   render (){
+
     return (
       <div className="App">
         <header className="App-header">
           <RiNeteaseCloudMusicFill className="App-logo" /> 
         </header>
         <main>
-          <PlaylistsContainer />
+          <PlaylistsContainer
+            playlist={this.state.allPlayLists}
+            playlistName = {this.state.playlistName}
+            handleFieldChange = {this.handleFieldChange}
+          />
+    
           <Playlist />
         </main>
         <footer>
--- semana7/labefy/src/components/PlaylistsContainer.js
+++ semana7/labefy/src/components/PlaylistsContainer.js
@@ -1,20 +1,53 @@
 import React from "react"
 import styled from "styled-components"
+import { IoIosArrowForward } from 'react-icons/io'
+import { IoCloseCircle } from 'react-icons/io5';
+import IconButton from './IconButton';
+import Playlist from "./Playlist";
 
 const Container = styled.div`
 width: 30%;
 height:800px;
 background-color:lightsalmon;
 `
+const PlayListDiv = styled.div`
+    background-color: lightgrey;
+    margin: 5px;
 
-const PlaylistsContainer = (props) =>{
-    return (
-        
-        <Container>
+`
 
-        </Container>
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100px;
+  justify-content: space-between;
+`
+
+export default class PlaylistsContainer extends React.Component {
+    
+    render (){
+
+        const playList = this.props.playlist.map((playlist) => {
+            return (
+              <PlayListDiv key={playlist.id}>{playlist.name}</PlayListDiv>
+            )
+          })
+  
+        return (
             
-    )
-}
+            <Container>
+                <h2>Playlists salvas</h2>
+                <input
+                  name="playlistName"
+                  value={this.props.playlistName}
+                  onChange = {this.props.handleFieldChange}
+                >
+                </input>
+                <button>Criar Playlist</button>
+                {playList}
 
-export default PlaylistsContainer
+            </Container>
+                
+        )
+    }
+}
