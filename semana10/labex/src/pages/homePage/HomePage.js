68a35079ee2d888e269134027ba9cbccf3e79349
tshadz <thais.sombra@hotmail.com>
2021-07-05 16:14:22 -0300

    :package: Implantação de carousel com bootstrap

--- semana10/labex/src/pages/homePage/HomePage.js
+++ semana10/labex/src/pages/homePage/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import CarouselContainer from '../../components/carousel/CarouselContainer'
 
 const HomePage = () => {
     return (<>
         <Header />
         <h2>Homepage</h2>
+        <CarouselContainer />
+        <div>Card Viagens Famosas</div>
+        <div>Saiba Mais</div>
         <Footer />
     </>
     )
