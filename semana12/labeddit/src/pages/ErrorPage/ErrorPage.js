cc3d9c46f5ffe89fe56d7c263fcc1722e232cb1a
tshadz <thais.sombra@hotmail.com>
2021-07-25 21:26:49 -0300

    :package: Requisição de posts

--- semana12/labeddit/src/pages/ErrorPage/ErrorPage.js
+++ semana12/labeddit/src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ErrorImage, ErrorPageContainer } from './styled'
+import Typography from '@material-ui/core/Typography'
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>ErrorPage</h1>
-        </div>
+        <ErrorPageContainer>
+            <ErrorImage/>
+            <Typography color={'primary'} variant={'h4'} align={'center'}>
+                Erro 404 - Página não encontrada
+            </Typography>
+        </ErrorPageContainer>
     )
 }
 
--- semana12/labeddit/src/pages/FeedPage/FeedPage.js
+++ semana12/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import useProtectedPage from '../../hooks/useProtectedPage'
+import useRequestData from '../../hooks/useRequestData'
+import { BASE_URL } from '../../constants/urls'
 
 const FeedPage = () => {
     useProtectedPage()
+    const posts = useRequestData([],`${BASE_URL}/posts`)
+    console.log('posts:',posts)
+
     return (
         <div>
             <h1>FeedPage</h1>
