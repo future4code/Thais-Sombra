d8c0351843b9730959a33c67258ce8b610e7afa8
tshadz <thais.sombra@hotmail.com>
2021-08-10 16:09:40 -0300

    :package: Exercicio 1

---
+++ semana15/typescript/src/exercicio1.ts
@@ -0,0 +1,9 @@
+// a) Crie uma variável **minhaString** do tipo `string` e atribua um valor a ela. Tente atribuir um número a esta variável. O que acontece?
+
+// let minhaString: string = "Hello World"
+// minhaString = 123
+
+// O typescript avisa que um nuúmero não pode ser atribuído a uma variável de tipo string
+
+
+
