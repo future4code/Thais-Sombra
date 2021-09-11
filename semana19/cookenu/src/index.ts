64823aab2b16bf4fbe133d35c1ae1f0b7c77e559
tshadz <thais.sombra@hotmail.com>
2021-09-10 21:43:28 -0300

    :arrow_up: resquests.rest

--- semana19/cookenu/src/index.ts
+++ semana19/cookenu/src/index.ts
@@ -1,3 +1,10 @@
 import app from "./app";
+import signup from "./endpoints/recipes/useres/signup";
 
-app.get("/");
+app.post("/users/signup", signup);
+app.post("/users/login");
+app.get("/users/profile");
+app.get("/users/:id/profile");
+
+app.post("/recipe");
+app.get("/recipe/:id");
