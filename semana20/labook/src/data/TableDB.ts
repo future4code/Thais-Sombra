3aba058279e34c3a9b927671e34875212f897ef5
tshadz <thais.sombra@hotmail.com>
2021-09-14 16:46:30 -0300

    :arrow_up: Reestruturaçao do arquivo migrations atraves da criaçao do TableDB

--- semana20/labook/src/data/TableDB.ts
+++ semana20/labook/src/data/TableDB.ts
@@ -2,10 +2,25 @@ import BaseDB from "./BaseDB";
 
 const printError = ( error: any ) => { console.log(error.sqlMessage || error.message )};
 
-export default class TableDataBase extends BaseDB {
+export default class TableDB extends BaseDB {
     public createTables = async () : Promise<void> => {
         await BaseDB.connection.raw(`
-           
+            CREATE TABLE IF NOT EXISTS labook_users(
+                id VARCHAR(255) PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL
+            );
+    
+            CREATE TABLE IF NOT EXISTS labook_posts(
+                id VARCHAR(255) PRIMARY KEY,
+                photo VARCHAR(255) NOT NULL,
+                description VARCHAR(255) NOT NULL,
+                type ENUM("normal","event") DEFAULT "normal",
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                author_id VARCHAR(255),
+                FOREIGN KEY (author_id) REFERENCES labook_users (id)
+            )
         `)
         .then(()=> { console.log("Tabelas criadas") })
         .catch(printError);
