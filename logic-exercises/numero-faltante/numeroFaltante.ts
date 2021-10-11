89396e88798704b21237baf2bf67bdb72f61c740
tshadz <thais.sombra@hotmail.com>
2021-10-11 19:20:55 -0300

    :checkered_flag: Estrutura inicial

---
+++ logic-exercises/numero-faltante/numeroFaltante.ts
@@ -0,0 +1,11 @@
+
+const array = [1,2,3,4,5,6,8,9,10]
+
+export const findMissingNumber = (arr: number[]): number => {
+    const expectedSum = 5050;
+    let sum = 0;
+    for (const num of arr) {
+      sum += num;
+    }
+    return expectedSum - sum;
+  };
